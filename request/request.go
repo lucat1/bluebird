4b527eaa136ce7b05bc90aaa3cd3a7400af5ed4e
gianmaria.rovelli2 <gianmaria.rovelli2@studio.unibo.it>
2022-10-19 10:44:47 +0200

    find userId by username added (wip)

--- request/request.go
+++ request/request.go
@@ -14,22 +14,41 @@ type transportWithHeader struct {
 }
 
 type tweet struct {
-	id   string
-	text string
+	ID       string
+	Text     string
+	AuthorID string
+	Geo      string
 }
-type tweetRawFromRespone struct {
+type user struct {
+	ID       string
+	Name     string
+	Username string
+}
+
+type userRawResponse struct {
+	ID       string `json:"id"`
+	Name     string
+	Username string
+}
+type responseFromUserAPI struct {
+	Data userRawResponse
+}
+
+type tweetRawResponse struct {
 	EditHistoryTweetIds []string `json:"edit_history_tweet_ids"`
-	Id                  string
+	ID                  string   `json:"id"`
 	Text                string
+	AuthorID            string `json:"author_id"`
+	Geo                 string
 }
 type metaTweet struct {
 	NextToken   string `json:"next_token"`
 	ResultCount int    `json:"result_count"`
-	NewestId    string `json:"newest_id"`
-	OldestId    string `json:"oldest_id"`
+	NewestID    string `json:"newest_id"`
+	OldestID    string `json:"oldest_id"`
 }
-type responseFromAPI struct {
-	Data []tweetRawFromRespone
+type responseFromTweetAPI struct {
+	Data []tweetRawResponse
 	Meta metaTweet
 }
 
@@ -46,8 +65,59 @@ func Init() {
 	client = &http.Client{Transport: &transportWithHeader{}}
 }
 
-func requestToAPI(url string) []tweet {
-	resp, err := client.Get(url)
+func requestToTweetAPI(link string, count int) []tweet {
+	var tweets []tweet
+	parsedUrl, err := url.Parse(link)
+	queries := parsedUrl.Query()
+	queries.Add("tweet.fields", "author_id,geo")
+	parsedUrl.RawQuery = queries.Encode()
+	link = parsedUrl.String()
+	fmt.Println(link)
+	if err != nil {
+		log.Panic("Errore richiesta twitter")
+	}
+	for len(tweets) < count {
+		resp, err := client.Get(link)
+		if err != nil {
+			log.Panic("Errore richiesta twitter")
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Panic("Errore richiesta twitter")
+		}
+
+		var jsonMap responseFromTweetAPI
+		err = json.Unmarshal([]byte(string(body)), &jsonMap)
+		if err != nil {
+			log.Panic("Errore richiesta twitter")
+		}
+		for i := 0; i < len(jsonMap.Data) && len(tweets) < count; i++ {
+			tweets = append(tweets, tweet{ID: jsonMap.Data[i].ID, Text: jsonMap.Data[i].Text, AuthorID: jsonMap.Data[i].AuthorID, Geo: jsonMap.Data[i].Geo})
+		}
+		if jsonMap.Meta.ResultCount == 0 || jsonMap.Meta.NextToken == "" {
+			break
+		}
+		queries := parsedUrl.Query()
+		queries.Set("pagination_token", jsonMap.Meta.NextToken)
+		parsedUrl.RawQuery = queries.Encode()
+		link = parsedUrl.String()
+	}
+	fmt.Println(len(tweets))
+	return tweets
+}
+
+func requestToUserAPI(link string) user {
+	var userResp user
+	parsedUrl, err := url.Parse(link)
+	queries := parsedUrl.Query()
+	queries.Add("tweet.fields", "author_id,geo")
+	parsedUrl.RawQuery = queries.Encode()
+	link = parsedUrl.String()
+	if err != nil {
+		log.Panic("Errore richiesta twitter")
+	}
+	resp, err := client.Get(link)
 	if err != nil {
 		log.Panic("Errore richiesta twitter")
 	}
@@ -57,26 +127,31 @@ func requestToAPI(url string) []tweet {
 		log.Panic("Errore richiesta twitter")
 	}
 
-	var jsonMap responseFromAPI
+	var jsonMap responseFromUserAPI
 	err = json.Unmarshal([]byte(string(body)), &jsonMap)
 	if err != nil {
 		log.Panic("Errore richiesta twitter")
 	}
-	var tweets []tweet
-	for i := 0; i < len(jsonMap.Data); i++ {
-		tweets = append(tweets, tweet{id: jsonMap.Data[i].Id, text: jsonMap.Data[i].Text})
-	}
-	return tweets
+	userResp = user{ID: jsonMap.Data.ID, Name: jsonMap.Data.Name, Username: jsonMap.Data.Username}
+	return userResp
 }
 
-func recentByKeyword(keyword string) []tweet {
-	return requestToAPI("https://api.twitter.com/2/tweets/search/recent?query=" + url.QueryEscape(keyword))
+func recentsByKeyword(keyword string, count int) []tweet {
+	return requestToTweetAPI("https://api.twitter.com/2/tweets/search/recent?query="+url.QueryEscape(keyword), count)
 }
-func tweetByUserId(userId string) []tweet {
-	return requestToAPI("https://api.twitter.com/2/users/" + url.QueryEscape(userId) + "/tweets")
+func tweetsByUserID(userID string, count int) []tweet {
+	return requestToTweetAPI("https://api.twitter.com/2/users/"+url.QueryEscape(userID)+"/tweets", count)
+}
+func tweetsByUsername(username string, count int) []tweet {
+	userID := userIDByUsername(username)
+	return requestToTweetAPI("https://api.twitter.com/2/users/"+url.QueryEscape(userID)+"/tweets", count)
+}
+func userIDByUsername(username string) string {
+	return requestToUserAPI("https://api.twitter.com/2/users/by/username/" + url.QueryEscape(username)).ID
 }
 
 func Test() {
-	recentByKeyword("#eredita")
-	tweetByUserId("2244994945")
+	// recentsByKeyword("#eredita", 24)
+	// tweetsByUserID("2244994945", 49)
+	// tweetsByUsername("TwitterDev", 20)
 }
