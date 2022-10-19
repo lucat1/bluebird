1c09543435c52fbe31dada68311787e25e436a95
Luca <luca.tagliavini5@studio.unibo.it>
2022-10-19 16:39:35 +0200

    wrote tests for tweet struct conversions
    Co-authored-by: Gianmaria Rovelli <gianmaria.rovelli2@studio.unibo.it>

--- request/structures_test.go
+++ request/structures_test.go
@@ -32,3 +32,20 @@ func TestUsers(t *testing.T) {
 		assert.Equal(t, user.ProfileImage, u.ProfileImageURL, "Expected ProfileImages to match")
 	}
 }
+
+func TestTweets(t *testing.T) {
+	tweets, err := response.Tweets()
+	if err != nil {
+		t.Errorf("Did not expect an error during conversion: %e", err)
+	}
+	if len(tweets) <= 0 {
+		t.Errorf("Expected a non-empty users list")
+	}
+	for i, tw := range response.Data {
+		tweet := tweets[i]
+		assert.Equal(t, tweet.ID, tw.ID, "Expected IDs to match")
+		assert.Equal(t, tweet.Text, tw.Text, "Expected Texts to match")
+		assert.Equal(t, tweet.User.ID, tw.AuthorID, "Expected User.ID to match")
+		assert.Equal(t, tweet.Geo, tw.Geo, "Expected Geos to match")
+	}
+}
