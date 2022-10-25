28fcf630a62bb8979b5b3b809a3ca06cc5e222ae
Gaia <gaia.clerici.01@gmail.com>
2022-10-25 16:16:26 +0200

    Center search and searchtype

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -21,21 +21,23 @@ const Search: React.FC = () => {
   }
   return (
     <>
-      <div className="flex flex-row items-center justify-center">
-        <label htmlFor="type" className="sr-only block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select a search type</label>
-        <select
-          id="type"
-          className="mx-4 w-48 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          onChange={e => setInputType(e.target.value)}
-          value={inputType}
-        >
-          {searchTypes.map(type =>
-            <option key={type}>{type}</option>
-          )}
-        </select>
-
-        <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Your Email</label>
-        <div className="relative col-start-3 col-span-2">
+      <div className="flex flex-row items-center justify-center grid grid-cols-5 gap-2">
+        <div className="col-start-2 col-span-1 m-2">
+          <label htmlFor="type" className="sr-only block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select a search type</label>
+          <select
+            id="type"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChange={e => setInputType(e.target.value)}
+            value={inputType}
+          >
+            {searchTypes.map(type =>
+              <option key={type}>{type}</option>
+            )}
+          </select>
+        </div>
+       
+        
+        <div className="relative col-start-3 col-span-2 m-3">
           <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
           </div>
@@ -50,6 +52,7 @@ const Search: React.FC = () => {
           />
           <button type="submit" onClick={handleSubmit} className="text-white absolute right-2.5 bottom-2.5 bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Search</button>
         </div>
+        
       </div>
 
       {
