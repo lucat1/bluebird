df0b77dfbfe29016a3f79d129ed424da0145f112
Gaia Clerici <gaia.clerici@studio.unibo.it>
2022-10-27 12:14:21 +0200

    FIxing grids

--- src/components/tweet-map.tsx
+++ src/components/tweet-map.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { useQuery } from "@tanstack/react-query";
-import {LatLng} from 'leaflet'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLng } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import fetch from "../fetch";
 import type { Tweet } from "../types";
- 
+
 export interface TweetProps {
   type: string;
   query: string;
@@ -20,18 +20,29 @@ const TweetList: React.FC<TweetProps> = ({ type, query }) => {
     { suspense: true }
   );
 
-  const position = [51.505, -0.09]
+  const position = [51.505, -0.09];
   return (
-  <MapContainer className="m-auto shadow-lg" style={{ width: '80vw', height: '70vh' }} center={new LatLng(position[0], position[1])} zoom={13} scrollWheelZoom={true}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={new LatLng(position[0], position[1])}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+    <MapContainer
+      className="m-auto shadow-lg"
+      style={{ width: "80vw", height: "70vh" }}
+      center={new LatLng(position[0], position[1])}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {tweets?.map(
+        (tweet) =>
+          tweet.geo && (
+            <Marker
+              position={
+                new LatLng(tweet.geo.coordinates[0], tweet.geo.coordinates[1])
+              }
+            >
+              <Popup>{tweet.text}</Popup>
+            </Marker>
+          )
+      )}
+    </MapContainer>
   );
 };
 
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -20,72 +20,73 @@ const Search: React.FC = () => {
 
   return (
     <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex items-center justify-center grid grid-cols-5 gap-2 mb-4"
-      >
-        <div className="col-start-2 col-span-1 m-2">
-          <label
-            htmlFor="type"
-            className="sr-only block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-          >
-            Search type
-          </label>
-          <select
-            id="type"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            {...register("type", { required: true })}
-          >
-            {searchTypes.map((type) => (
-              <option key={type}>{type}</option>
-            ))}
-          </select>
-          {/* {errors.type?.message && <label className="red">{errors.type?.message}</label>} */}
-        </div>
-        <div className="col-start-3 col-span-2 m-3 row-span-1">
-        <div className="relative">
-          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+      <div className="flex justify-center">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid grid-cols-[auto_1fr] gap-4 my-4 max-w-4xl"
+        >
+          <div className="flex items-center">
+            <label
+              htmlFor="type"
+              className="sr-only block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
-            </svg>
+              Search type
+            </label>
+            <select
+              id="type"
+              className="w-32 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              {...register("type", { required: true })}
+            >
+              {searchTypes.map((type) => (
+                <option key={type}>{type}</option>
+              ))}
+            </select>
+            {errors.type?.message && (
+              <label className="red">{errors.type?.message}</label>
+            )}
           </div>
-          <label
-            htmlFor="query"
-            className="sr-only block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-          >
-            Query
-          </label>
-          <input
-            id="query"
-            type="search"
-            className="mb-2 block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
-            placeholder="Search"
-            {...register("query", { required: true })}
-          />
-          <button
-            type="submit"
-            className="text-white absolute right-2.5 bottom-2.5 bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
-          >
-            Search
-          </button>
-          
-        </div>
-        {errors.query && <label className="text-red-800 dark:text-red-500 col-start-5 p-3 text-sm italic row-span-1">Type in something</label>}
-        </div>
-      </form>
 
+          <div className="relative">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
+              </svg>
+            </div>
+            <label
+              htmlFor="query"
+              className="sr-only block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Query
+            </label>
+            <input
+              id="query"
+              type="search"
+              className="block p-4 pl-10 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
+              placeholder="Search"
+              {...register("query", { required: true })}
+            />
+            {errors.query && "blablabla"}
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
       <React.Suspense fallback={<Loading />}>
         {props.query != "" && <TweetList {...props} />}
       </React.Suspense>
