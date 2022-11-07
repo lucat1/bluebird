67fca366035f8a05b188271827513a3e7c7a8912
Luca <luca.tagliavini5@studio.unibo.it>
2022-11-07 17:43:21 +0100

    rollback to react 17 to fix the map

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./components/app";
@@ -6,10 +6,11 @@ import Search from "./search";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <App>
       <Search />
     </App>
-  </QueryClientProvider>
+  </QueryClientProvider>,
+  document.getElementById("root")!
 );
