1e251143085aecb696fba0607496f1f555dea412
Andreea Scrob <andreea.scrob@studio.unibo.it>
2022-11-10 21:21:42 +0100

    change colors of the cake

--- src/components/tweet-cake.tsx
+++ src/components/tweet-cake.tsx
@@ -10,10 +10,10 @@ interface TweetCakeProps {
 const TweetCake: React.FC<TweetCakeProps> = ({ tweets }) => {
   //to edit with dataset in input
   const dataset = [
-    { title: Sentiment.Anger, value: 4, color: '#E38627' },
-    { title: Sentiment.Sadness, value: 4, color: '#C13C37' },
+    { title: Sentiment.Anger, value: 4, color: '#0c4a6e' },
+    { title: Sentiment.Sadness, value: 4, color: '#a16207' },
     { title: Sentiment.Fear, value: 4, color: '#6A2135' },
-    { title: Sentiment.Joy, value: 4, color: '#32CD32' }
+    { title: Sentiment.Joy, value: 4, color: '#047857' }
   ]
   const [selected, setSelected] = React.useState<number | undefined>(undefined);
   const [hovered, setHovered] = React.useState<number | undefined>(undefined);
@@ -21,7 +21,7 @@ const TweetCake: React.FC<TweetCakeProps> = ({ tweets }) => {
     if (hovered === i) {
       return {
         ...entry,
-        color: 'grey',
+        color: '#0ea5e9',
       };
     }
     return entry;
