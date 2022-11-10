983447f9344e60480817e482e602411fbdd603be
Gaia <gaia.clerici@studio.unibo.it>
2022-11-10 12:28:20 +0100

    Fixed navbar

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,10 +6,14 @@ import Loading from "./loading";
 import Error from './error'
 import { queryClient } from "../main";
 
-const App: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
+
+const App: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  const [open, setOpen] = React.useState(false)
+  
+  return (
   <OverlayContainer>
     <main className="w-screen h-screen dark:bg-gray-900 dark:text-gray-800 dark:text-white overflow-auto">
-      <nav className="flex flex-row justify-between bg-sky-800 border-gray-200 py-2 px-4 lg:px-8 dark:bg-gray-900">
+      <nav className="flex flex-wrap justify-between bg-sky-800 border-gray-200 py-2 px-4 lg:px-8 dark:bg-gray-900">
           <a href="/" className="flex items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -157,18 +161,21 @@ const App: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
               </g>
             </svg>
           </a>
+          <button onClick={_ => setOpen(!open)} className="inline-flex justify-center items-center ml-3 text-white rounded-lg md:hidden hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500" aria-controls="mobile-menu-2" aria-expanded="false">
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
 
+          </button>           
 
-            <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-              <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <div className={`${open ? '' : 'hidden'} w-full w-full md:block md:w-auto`}>
+              <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-900 md:dark:bg-gray-900 dark:border-gray-600">
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Eredità</a>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-900 rounded md:text-white hover:bg-sky-700 hover:text-white md:hover:bg-transparent md:hover:text-orange-300 md:p-0 md:dark:hover:text-sky-600 dark:text-gray-400 dark:hover:bg-sky-600 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Eredità</a>
               </li>
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Reazione a Catena</a>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-900 rounded md:text-white hover:bg-sky-700 hover:text-white md:hover:bg-transparent md:hover:text-orange-300 md:p-0 md:dark:hover:text-sky-600 dark:text-gray-400 dark:hover:bg-sky-600 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Reazione a Catena</a>
               </li>
               <li>
-                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Scacchi</a>
+                <a href="#" className="block py-2 pr-4 pl-3 text-gray-900 rounded md:text-white hover:bg-sky-700 hover:text-white md:hover:bg-transparent md:hover:text-orange-300 md:p-0 md:dark:hover:text-sky-600 dark:text-gray-400 dark:hover:bg-sky-600 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Scacchi</a>
               </li>
             </ul>
 
@@ -184,5 +191,5 @@ const App: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
     </main>
   </OverlayContainer>
 );
-
+}
 export default App;
