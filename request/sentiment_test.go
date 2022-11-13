0066873fb17e826e8e9b9a1f85573a80736d6ca6
Francesco Apollonio <apollonio_francesco@libero.it>
2022-11-13 18:38:36 +0200

    Test SentimentsFromTweet

--- request/sentiment_test.go
+++ request/sentiment_test.go
@@ -2,14 +2,20 @@ package request
 
 import (
 	"testing"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRandomSentiment (t *testing.T) {
-
   A := randomSentiment()
-  
   assert.Equal(t,A[0]+A[1]+A[2]+A[3],float32(1),"Random Sentiment Values do not add up to 1")
+}
 
+func TestSentimentsFromTweet(t *testing.T){
+	tweet := Tweet{
+		Text: "Ciao mamma sto testando :D",
+	}
+	retValue, err := SentimentsFromTweet(tweet)
+	if err != nil{
+	assert.EqualValues(t, rawSentimentResponse.Sentiments, retValue, "Returned sentiment is not as expected")
+	}
 }
