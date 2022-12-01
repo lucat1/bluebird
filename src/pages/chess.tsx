cf99b06d65d9f5de4249a864fce7d4ced8f17775
Francesco Apollonio <apollonio_francesco@libero.it>
2022-12-01 18:04:25 +0200

    Timer selector updated

--- src/pages/chess.tsx
+++ src/pages/chess.tsx
@@ -113,6 +113,10 @@ const Chess: React.FC = () => {
       )}
       {!code && (
         <div className="flex flex-1 items-center justify-center flex-col">
+          <div className="row flex items-center mb-20"> 
+            <label className="text-sm font-medium text-gray-900 dark:text-white"> 
+            Benvenuto nella modalità scacchi! Seleziona il tempo che vuoi rendere disponibile per ogni mossa.</label>
+          </div>
           <form
             className="flex flex-col"
             onSubmit={handleSubmit((data) => {
@@ -126,32 +130,33 @@ const Chess: React.FC = () => {
               }
             })}
           >
-            <div className="flex flex-col justify-center items-center">
-              <div className="flex flex-row my-2">
-                <input
-                  type="number"
-                  placeholder="hours"
-                  className="mx-2 rounded-lg text-center text-black"
-                  defaultValue={0}
-                  max={100}
-                  min={0}
-                  {...register("hours", {
-                    required: true,
-                    valueAsNumber: true,
-                  })}
-                />
-                <input
-                  type="number"
-                  placeholder="minutes"
-                  className="mx-2 rounded-lg text-center text-black"
-                  defaultValue={1}
-                  max={59}
-                  min={0}
-                  {...register("minutes", {
-                    required: true,
-                    valueAsNumber: true,
-                  })}
-                />
+            <div className="w-full flex flex-col justify-center items-center">
+              <div className="flex-row">
+              <label className="block mb-2 text-sm text-center font-medium text-gray-900 dark:text-white">Seleziona qui le ore</label>
+                  <input
+                    type="number"
+                    placeholder="Ore"
+                    className="w-full mb-2 rounded-lg text-center dark:bg-gray-700"
+                    max={100}
+                    min={0}
+                    {...register("hours", {
+                      required: true,
+                      valueAsNumber: true,
+                    })} />
+
+                <div className="flex-row">
+                  <label className="block mb-2 text-sm text-center font-medium text-gray-900 dark:text-white">Seleziona qui i minuti</label>
+                  <input
+                    type="number"
+                    placeholder="Minuti"
+                    className="w-full mb-2 rounded-lg text-center dark:bg-gray-700"
+                    max={59}
+                    min={0}
+                    {...register("minutes", {
+                      required: true,
+                      valueAsNumber: true,
+                    })} />
+                </div>
               </div>
               {errors.minutes && (
                 <label htmlFor="hours">
@@ -168,9 +173,9 @@ const Chess: React.FC = () => {
             <div className="flex justify-center">
               <button
                 type="submit"
-                className="w-1/2 justify-center text-white text-center hover:bg-sky-700 bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+                className="w-full mt-5 justify-center text-white text-center hover:bg-sky-700 bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
               >
-                Inizio!
+                Inizia la partita!
               </button>
             </div>
           </form>
