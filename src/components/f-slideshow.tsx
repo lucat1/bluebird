01de8bb6086cbe5b2274daae900ec43521b17194
Gaia <gaia.clerici@studio.unibo.it>
2022-12-15 13:24:21 +0100

    fantacitorio style

--- src/components/f-slideshow.tsx
+++ src/components/f-slideshow.tsx
@@ -11,7 +11,7 @@ const Slideshow: React.FC<{teams:Team[]}> = (props) => {
       
       <div className="border border-sky-500 overflow-none w-screen xl:w-96">
       {
-            filteredTeams.length == 0 ? <div className="overflow-none xl:max-w-xl xl:w-fit w-screen h-96 justify-center text-red-500">Errore
+            filteredTeams.length == 0 ? <div className="overflow-none xl:max-w-xl xl:w-fit w-screen h-96 justify-center text-red-500 text-2xl text-center flex items-center justify-center"> <span className="nline-block align-middle">Non esistono squadre create da quell'utente</span> 
             </div> : <Carousel>
             {filteredTeams.map((team) => (
                 <div key={team.username} className="overflow-none xl:max-w-xl xl:w-fit w-screen h-96">
@@ -22,13 +22,13 @@ const Slideshow: React.FC<{teams:Team[]}> = (props) => {
             </Carousel>
           }
       </div>
-      <div className="flex flex-row border border-sky-500 p-1 justify-between">
+      <div className="flex flex-row border border-sky-500 p-1 justify-center">
       <input
               onChange={(e)=>{setName(e.target.value)}}
               id="query"
               type="search"
               className="block p-4 hover:border-gray-400   text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
-              placeholder="Search"
+              placeholder="Search username"
               // {...register("query", { required: true })}
             />
       </div>
